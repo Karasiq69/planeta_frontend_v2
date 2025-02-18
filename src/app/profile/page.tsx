0e13fe589fba1e70dd533a6cd62c6c1ca12a21da
karasiq69 <0982646@mail.ru>
2025-02-18 18:22:23 +0300

    fix clf

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,12 +3,11 @@ import {useAuth} from "@/providers/AuthProvider";
 
 type Props = {};
 const Page = (props: Props) => {
-    const {user, isLoading} = useAuth()
+    // const {user, isLoading} = useAuth()
     return (
         <div>
             <h1>Profile</h1>
-            {JSON.stringify(user)}
-            <div>Welcome back, {!isLoading && user?.role}</div>
+
         </div>
     );
 };
